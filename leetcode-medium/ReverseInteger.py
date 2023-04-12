bf84b08b1c8dc741a1fe56aab07f85cfdfe67cf8
Daniella Omenogor <daniellaomenogor@gmail.com>
2023-04-12 22:06:38 +0000

    Import module for bit manipulation approach

--- leetcode-medium/ReverseInteger.py
+++ leetcode-medium/ReverseInteger.py
@@ -3,6 +3,9 @@
     If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
 """
 
+import math
+
+
 class Solution:
     def reverse(self, x: int) -> int:
         res = 0
@@ -13,14 +16,14 @@ class Solution:
             return x
         elif x < 0:
             x = abs(x)
-            isNeg = 1  # change value of isNeg since x is negative
+            isNeg = 1
         
         while x:
             temp = x % 10
-            res = res * 10 + temp 
+            res = res * 10 + temp
             x = x // 10
 
-        if -2**31 <= res <= 2**31: # check if number is within given range
+        if -2**31 <= res <= 2**31:
             if isNeg:
                 return -res
             else:
@@ -28,6 +31,28 @@ class Solution:
                 # return (x_dup // abs(x_dup)) * res
         else:
             return 0
+
+        # Solution using Bit Manipulation
+        # def reverse(self, x: int) -> int:
+        # # Integer.MAX_VALUE = 2147483647 (end with 7)
+        # # Integer.MIN_VALUE = -2147483648 (end with -8 )
+
+        # MIN = -2147483648  # -2^31,
+        # MAX = 2147483647  #  2^31 - 1
+
+        # res = 0
+        # while x:
+        #     digit = int(math.fmod(x, 10))  # in Python, -1 %  10 = 9
+        #     x = int(x / 10)  # in Python, -1 // 10 = -1
+
+        #     if res > MAX // 10 or (res == MAX // 10 and digit > MAX % 10):
+        #         return 0
+        #     if res < MIN // 10 or (res == MIN // 10 and digit < MIN % 10):
+        #         return 0
+        #     res = (res * 10) + digit
+
+        # return res
+
 
 # Examples
 s = Solution()
