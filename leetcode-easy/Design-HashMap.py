ffabdb3881909b40dcc22888c891d9ee5347fff6
Daniella Omenogor <daniellaomenogor@gmail.com>
2023-04-17 00:55:40 +0000

    Add LinkedList implementation for 'Design HashMap'

--- leetcode-easy/Design-HashMap.py
+++ leetcode-easy/Design-HashMap.py
@@ -9,33 +9,61 @@ Implement the MyHashMap class:
 - int get(int key) returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key.
 - void remove(key) removes the key and its corresponding value if the map contains the mapping for the key.
 """
+class ListNode:
+    def __init__(self, key=-1, val=-1, next=None):
+        self.key = key
+        self.val = val
+        self.next = next
+
 
 class MyHashMap:
     # FIRST APPROACH USING LINKED LISTS & CHAINING
-    # def __init__(self):
-        
-
-    # def put(self, key: int, value: int) -> None:
-        
-
-    # def get(self, key: int) -> int:
-        
-
-    # def remove(self, key: int) -> None:
-
-    # SECOND APPROACH USING LIST
     def __init__(self):
-        self.l = [-1] * 1000001 # initialise hashmap with default values of -1
+        self.map = [ListNode() for i in range(1000)]
 
+    def hash(self, key):
+        return key % len(self.map)
+    
     def put(self, key: int, value: int) -> None:
-        self.l[key] = value
+        cur = self.map[self.hash(key)]
+        while cur.next:
+            if cur.next.key == key:
+                cur.next.val = value
+                return
+            cur = cur.next
+        cur.next = ListNode(key, value)
 
     def get(self, key: int) -> int:
-        return self.l[key]
+        cur = self.map[self.hash(key)].next
+        while cur:
+            if cur.key == key:
+                return cur.val
+            cur = cur.next
+        return -1
 
     def remove(self, key: int) -> None:
-        self.l[key] = -1 # removing the key == changing thekey's value to default value of -1
+        cur = self.map[self.hash(key)]
+        while cur and cur.next:
+            if cur.next.key == key:
+                cur.next = cur.next.next
+                return
+            cur = cur.next
 
+
+    # SECOND APPROACH USING LIST (PRETTY INEFFICIENT)
+    # def __init__(self):
+    #     self.l = [-1] * 1000001 # initialise hashmap with default values of -1
+
+    # def put(self, key: int, value: int) -> None:
+    #     self.l[key] = value
+
+    # def get(self, key: int) -> int:
+    #     return self.l[key]
+
+    # def remove(self, key: int) -> None:
+    #     self.l[key] = -1 # removing the key == changing thekey's value to default value of -1
+
+    # TODO: Try using buckets to make first approach more efficient
 
 # Your MyHashMap object will be instantiated and called as such:
 # obj = MyHashMap()
