35e3b0cc9ca847184541ba9b52306de57fdb9f12
Daniella Omenogor <daniellaomenogor@gmail.com>
2023-04-18 23:35:39 +0000

    Initial commit

---
+++ leetcode-easy/First-Unique-Character-in-a-String.py
@@ -0,0 +1,16 @@
+""" Prompt: Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1."""
+
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        pass
+
+
+# Examples
+example = Solution()
+
+# Example 1:
+s = "Leetcode"
+print(example.firstUniqChar(s))
+# Output: 0
+# Explanation: L is the first unique element in the string
