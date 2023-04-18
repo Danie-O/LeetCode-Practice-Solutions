642ae8e4ae19344f3ba4fbf9943bd5ed60b45e85
Daniella Omenogor <daniellaomenogor@gmail.com>
2023-04-18 23:21:08 +0000

    First solution

--- leetcode-easy/Intersection-of-Two-Arrays.py
+++ leetcode-easy/Intersection-of-Two-Arrays.py
@@ -8,4 +8,15 @@ Each element in the result must be unique and you may return the result in any o
 
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        pass
+        # CONCISE SOLUTION.
+        return list(set(nums1) & set(nums2))  # use logical AND property
+
+
+# Examples
+example = Solution()
+
+# Example 1:
+nums1 = [1, 2, 2, 1]
+nums2 = [2, 2]
+print(example.intersection(nums1, nums2))
+# Output: true
