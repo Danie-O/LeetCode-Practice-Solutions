43b461bf2a067f08eb47c0cc64368c9ec526053e
Daniella Omenogor <daniellaomenogor@gmail.com>
2023-04-18 23:44:06 +0000

    Add solution and notes

--- leetcode-easy/First-Unique-Character-in-a-String.py
+++ leetcode-easy/First-Unique-Character-in-a-String.py
@@ -1,9 +1,32 @@
-""" Prompt: Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1."""
+from collections import Counter
+
+""" Prompt: Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1. """
 
 
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        pass
+        # create counter of unique characters and their count
+        counter = Counter(s)
+
+        for i in range(len(s)):
+            if counter[s[i]] == 1:
+                # if count of element is 1, it's unique
+                return i    # return its index
+
+        return -1   # return -1, if there is no unique character in input string
+
+
+""" Note:
+    I saw a solution which uses s.rindex and index to determine if the last seen occurrence of the character is the same as its first occurence. 
+    Interesting  way of looking at this question in my opinion.
+    See solution below:
+
+    def firstUniqChar(self, s: str) -> int:
+        for i in s:
+            if s.rindex(i) == s.index(i):
+                return s.index(i)            
+        return -1
+"""
 
 
 # Examples
