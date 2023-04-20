c85f4c852f4dbd2d73c7a440af222d66d11241cc
Daniella Omenogor <daniellaomenogor@gmail.com>
2023-04-20 23:52:20 +0000

    Initial commit

---
+++ leetcode-easy/No-Of-Good-Pairs.py
@@ -0,0 +1,14 @@
+from typing import List
+
+""" PROMPT: Given an array of integers nums, return the number of good pairs.
+
+    A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+"""
+
+
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        # Solution 1: nested loop
+        # Solution 2: sorting + two pointers
+        # Solution 3: using a list
+        pass
