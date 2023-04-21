29e9f9693e680172df1acdc2d48b9e50679b3913
Daniella Omenogor <daniellaomenogor@gmail.com>
2023-04-21 00:39:43 +0000

    Implement second approach

--- leetcode-easy/No-Of-Good-Pairs.py
+++ leetcode-easy/No-Of-Good-Pairs.py
@@ -9,12 +9,25 @@ from typing import List
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
         # Solution 1: nested loop
-        count = 0
-        for i in range(len(nums) - 1):
-            for j in range(i+1, len(nums)):
-                if nums[i] == nums[j]:
-                    count += 1
-        return count
+        # pairs = 0
+        # for i in range(len(nums) - 1):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i] == nums[j]:
+        #             pairs += 1
+        # return pairs
+
         # Solution 2: sorting + two pointers
+        l, r = 0, 1
+        pairs = 0
+        nums.sort()  # sort list in-place
+
+        while r < len(nums):
+            if nums[l] == nums[r]:
+                pairs += r - l
+            else:
+                l = r
+            r += 1
+        return pairs
+
         # Solution 3: using collections.Counter
         pass
