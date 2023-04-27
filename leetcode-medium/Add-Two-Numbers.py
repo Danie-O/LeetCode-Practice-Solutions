67559e88773e74a0d61f150d75d09a5c85459b11
Daniella Omenogor <daniellaomenogor@gmail.com>
2023-04-27 00:11:00 +0000

    Refactor to improve speed

--- leetcode-medium/Add-Two-Numbers.py
+++ leetcode-medium/Add-Two-Numbers.py
@@ -12,28 +12,27 @@ class ListNode:
         self.val = val
         self.next = nxt
 
-        
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode()
-        current = dummy
-
+        current = dummy = ListNode()
         carry = 0
-        while l1 or l2 or carry:
+
+        while l1 or l2:
             v1 = l1.val if l1 else 0
             v2 = l2.val if l2 else 0
 
             #calculate sum
             sumval = v1 + v2 + carry
             carry = sumval // 10
-            sumval = sumval % 10
-            current.next = ListNode(sumval)
+            current.next = ListNode(sumval % 10)
 
             #update pointers
             current = current.next
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
         
+        if carry:
+            current.next = ListNode(carry)
         return dummy.next
     
 
