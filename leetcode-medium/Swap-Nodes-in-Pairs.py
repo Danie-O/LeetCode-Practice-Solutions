e4e56cb9a263d488f6c00669e5ab574b63b1254e
Daniella Omenogor <daniellaomenogor@gmail.com>
2023-05-01 08:00:33 +0000

    Initial commit

--- leetcode-medium/Swap-Nodes-in-Pairs.py
+++ leetcode-medium/Swap-Nodes-in-Pairs.py
@@ -2,3 +2,14 @@
 
     You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
 """
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        pass
