ddad068a146cc47f21c186e4eb7bad226836648c
Daniella Omenogor <daniellaomenogor@gmail.com>
2023-05-11 00:13:15 +0000

    Push solution

--- leetcode-easy/Subtree-Of-Another-Tree.py
+++ leetcode-easy/Subtree-Of-Another-Tree.py
@@ -1,6 +1,8 @@
 from typing import Optional
-""" PROMPT:
-
+""" PROMPT: Given the roots of two binary trees root and subRoot, 
+    return true if there is a subtree of root with the same structure and node values of subRoot and false otherwise.
+    A subtree of a binary tree tree is a tree that consists of a node in tree and all of this node's descendants. 
+    The tree tree could also be considered as a subtree of itself.
 """
 
 # Definition for a binary tree node.
@@ -11,5 +13,24 @@ class TreeNode:
         self.right = right
 
 class Solution:
-    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        pass
+    """ Time complexity: O(s + t) """
+    def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
+        if not t: 
+            # an empty subtree is a subtree of the other if tree is empty or not
+            return True
+        if not s:
+            # t cannot be subtree of an empty tree(s) if t is non-empty 
+            return False
+
+        # recall that the tree(s) is considered a subtree of itself 
+        if self.sameTree(s, t):
+            return True
+        return self.isSubtree(s.left, t) or self.isSubtree(s.right, t)
+
+    # helper function to check if subtree is the same as main tree
+    def sameTree(self, s, t):
+        if not s and not t:
+            return True
+        if s and t and s.val == t.val:
+            return self.sameTree(s.left, t.left) and self.sameTree(s.right, t.right)
+        return False
