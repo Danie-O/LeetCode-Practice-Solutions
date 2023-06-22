5a4d4e6af28ea53f25a7eb9a23b0c95dc187f113
Daniella Omenogor <daniellaomenogor@gmail.com>
2023-06-22 19:45:02 +0000

    Max subarray w/o mutating input array

--- leetcode-medium/Max-Subarray.py
+++ leetcode-medium/Max-Subarray.py
@@ -3,16 +3,31 @@ class Solution:
     #               where N=length of input array
     # SPACE: O(1) => No extra memory is used as current max sum at each index is plugged back into the input 
 
+    # USING KADANE'S ALGO. AND MUTATING THE INPUT ARRAY
+    # def maxSubArray(self, nums: List[int]) -> int:
+    #     overall_max_sum = nums[0]
+
+    #     for i in range(1, len(nums)):
+    #         # for each element, update its value such that: 
+    #         # the max sum is either its value or its value + the max sum of previous elements
+    #         nums[i] = max(nums[i], nums[i] +  nums[i-1])
+
+    #         # update the overall max_sum if current max_sum > than the overall_max_sum
+    #         overall_max_sum = max(overall_max_sum, nums[i])
+    #     return overall_max_sum
+
+    # USING KADANE'S ALGO. WITHOUT MUTATING THE INPUT ARRAY
     def maxSubArray(self, nums: List[int]) -> int:
         overall_max_sum = nums[0]
-
-        for i in range(1, len(nums)):
-            # for each element, update its value such that: 
-            # the max sum is either its value or its value + the max sum of previous elements
-            nums[i] = max(nums[i], nums[i] +  nums[i-1])
+        current_sum = 0
+        for num in nums:
+            # do not consider negative predecessor for positive number
+            if current_sum < 0:
+                current_sum = 0
+            current_sum = max(num, num +  current_sum)
 
             # update the overall max_sum if current max_sum > than the overall_max_sum
-            overall_max_sum = max(overall_max_sum, nums[i])
+            overall_max_sum = max(overall_max_sum, current_sum)
         return overall_max_sum
 
         
