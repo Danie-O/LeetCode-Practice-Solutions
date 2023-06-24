a487e95ca47ed1d3c15f4efadc189486cb8ac6f2
Daniella Omenogor <daniellaomenogor@gmail.com>
2023-06-24 00:35:05 +0000

    Majority element: O(n) time, O(1) space

--- leetcode-easy/MajorityElement.py
+++ leetcode-easy/MajorityElement.py
@@ -3,17 +3,27 @@ from typing import List
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        # Trying hash map: O(n) time, O(n) space
-        nums_map = {}
+        # Using hash map: O(n) time, O(n) space
+        # nums_map = {}
         
-        for num in nums:
-            nums_map[num] = nums_map.get(num, 0) + 1
-            if (len(nums) // 2) < nums_map[num]:
-                return num
+        # for num in nums:
+        #     nums_map[num] = nums_map.get(num, 0) + 1
+        #     if (len(nums) // 2) < nums_map[num]:
+        #         return num
 
         # for num in nums_map:
         #     if (len(nums)) // 2 < nums_map[num]:
         #         return num
+
+        # Time: O(n), Space: O(1)
+        majority_num = nums[0]
+        count = 0
+        for num in nums:
+            count += 1 if num == majority_num else -1
+            if count == 0:
+                majority_num = num
+                count = 1
+        return majority_num
 
 # Examples
 s = Solution()
