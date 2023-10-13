f1f4d6b7a1465e4650969fe6818be04533117a64
Daniella Omenogor <daniellaomenogor@gmail.com>
2023-10-13 22:12:50 +0000

    push solution

--- leetcode-medium/Number-Of-Islands.py
+++ leetcode-medium/Number-Of-Islands.py
@@ -28,4 +28,38 @@ def dfs(i, j, grid, rows, cols):
 
     return (top + left + bottom + right + 1)
 
+# BFS SOLUTION
+def numIslands(self, grid: List[List[str]]) -> int:
+    if not grid:
+        return 0
+
+    rows, cols = len(grid), len(grid[0])
+    visited = set()
+    islands = 0
+
+    def bfs(i, j):
+        queue = collections.deque((i, j))
+        visited.add((i, j))
+
+        while queue:
+            r, c = queue.popleft()
+
+            directions = [[1,0], [-1,0], [0,1], [0, -1]]
+            for dr, dc in directions:
+                row, col = r + dr, c + dc
+                if (row in range(rows) and 
+                    col in range(cols) and 
+                    grid[row][col] == "1" and 
+                    (row, col not in visited)):
+                    queue.append(row, col)
+                    visited.add((row, col))
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == "1" and (i, j) not in visited:
+                bfs(i, j)
+                islands += 1
+    return islands
+
+
         
